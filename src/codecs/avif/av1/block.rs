576596968cad5cbf999286246048d54e84df9c4a
appunni.m <31534711+appunni-m@users.noreply.github.com>
2026-09-03 08:37:20 +0530

    fix(av1): admit b16x32 inter-intra topology

--- src/codecs/avif/av1/block.rs
+++ src/codecs/avif/av1/block.rs
@@ -55766,7 +55766,6 @@ impl Lossy420Decoder {
                 && !quantization.segment_lossless
                 && any_child_split
                 && !all_children_split
-                && prediction_state.inter_intra.is_none()
                 && prediction_state.obmc.is_none())
             .then_some(())
             .portable()?;
